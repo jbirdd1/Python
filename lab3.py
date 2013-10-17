27c63497754ec03b51af07d0d97f1049728d7e03
Morton <morjes14@ccm-2610-d2.evergreen.edu>
2013-10-17 16:46:56 -0700

    lab3

---
+++ lab3.py
@@ -0,0 +1,26 @@
+# Name: Jessica Morton
+# Evergreen Login: morjes14
+# Computer Science Foundations
+# Programming as a Way of Life
+# Lab 3
+
+
+n = 3
+series = 'sum'
+
+if series == 'fibonacci':
+    x = 0
+    y = 1
+    for i in range(n-1):
+        fib = x + y
+        x = y 
+        y = fib
+    print fib
+
+if series == 'sum':
+    total = 0
+    x = 1
+    for i in range(n):
+        total = total + (3*x)
+        x = x + 1
+        print total
