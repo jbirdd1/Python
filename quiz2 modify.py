3f4e0163875d3b45b8d75ce40a8710988274735f
Morton <morjes14@ccm-2610-d1.evergreen.edu>
2013-10-31 16:20:01 -0700

    yup

--- quiz2 modify.py
+++ quiz2 modify.py
@@ -4,23 +4,30 @@
 # Programming as a Way of Life
 # Lab 3
 
-
 n = 10
 series = 'fibonacci'
 
-if series == 'fibonacci':
+def fib(n):
     x = 0
     y = 1
     for i in range(n-1):
         fib = x + y
         x = y 
         y = fib
-        print fib
-
-if series == 'sum':
+    return fib
+    
+def sum(n):
     total = 0
     x = 1
     for i in range(n):
         total = total + (3*x)
         x = x + 1
-        print total
+    return total
+
+if series =='fibonacci':
+    fibResult = fib(n)
+    print fibResult
+    
+if series == 'sum':
+    sumResult = sum(n)
+    print sumResult
